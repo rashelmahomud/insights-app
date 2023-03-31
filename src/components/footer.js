262ef0771b3027af9c09a3ffa81e97c1c2705a8a
MCTC <rashelmahmudraj1998@gmail.com>
2023-03-31 11:03:52 +0600

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,46 @@
 
+import { BsFacebook, BsInstagram, BsYoutube } from 'react-icons/bs';
+
 export default function footer() {
   return (
-    <div className="text-2xl font-bold my-10 text-center bg-gray-500">
-      <div>
-        <div className="xl:container xl:mx-auto grid lg:grid-cols-4">
+    <div className="text-2xl font-bold my-10 text-center bg-black">
+      <div className='xl:container xl:mx-auto'>
+        <div className=" grid lg:grid-cols-4 text-white p-10">
           <div>
             <h1>PennyPot</h1>
-            <p>Join the revolosion and see how easy middle maks home lose</p>
+            <p className="text-sm">Join the revolosion and see how easy middle maks home lose</p>
           </div>
 
           <div>
             <h2>Company</h2>
-            <p>Contant</p>
+            <p className="text-sm">Contant</p>
           </div>
-          <div></div>
-          <div></div>
 
+          <div>
+            <h3>Who we help</h3>
+            <ul className="text-sm">
+              <li>Customers</li>
+              <li>Brokars</li>
+              <li>Lenders</li>
+            </ul>
+
+          </div>
+
+          <div>
+            <h2>Follow Us On</h2>
+
+            <ul className='flex'>
+              <li><BsFacebook /></li>
+              <li><BsInstagram /></li>
+              <li><BsYoutube /></li>
+              <li><BsYoutube /></li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div>
+          <hr />
         </div>
       </div>
       This is a footer section
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { BsFacebook, BsInstagram, BsYoutube } from 'react-icons/bs';
 
 export default function header() {
     return (
